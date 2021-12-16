18a86e238e229833744fc887b198094c4675363a
“4slash271” <4slash271@gmail.com>
2021-12-17 04:34:51 +0900

    finished main-wrapper init swiper

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 $(".bars-wrapper").click(function(){
     $(".mb-navi-wrapper").toggleClass("active");
 })
+
+var swiper = new Swiper(".main-wrapper .swiper-container", {
+    loop:true,
+    effect:"fade",
+    speed: 1000,
+    autoplay:{
+        delay:5000, disableOnInteraction: false
+    },
+    navigation: {
+		nextEl: '.main-wrapper .bt-next',
+		prevEl: '.main-wrapper .bt-prev',
+	},
+})
+
