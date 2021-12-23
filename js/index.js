807e46a942c2f71b69cfa40e367dd1a00eb5656b
“4slash271” <4slash271@gmail.com>
2021-12-23 23:46:34 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 $(".bars-wrapper").click(function(){
     $(".mb-navi-wrapper").toggleClass("active");
 })
+$(".tour-wrapper .bt-buy").click(function(){
+    $(".modal-wrapper").toggleClass("active");
+    $(".modal-wrapper > .modal-wrap").css("transform","translateY(0)");
+})
 
 var swiper = new Swiper(".main-wrapper .swiper-container", {
     loop:true,
@@ -10,8 +14,8 @@ var swiper = new Swiper(".main-wrapper .swiper-container", {
         delay:5000, disableOnInteraction: false
     },
     navigation: {
-		nextEl: '.main-wrapper .bt-next',
-		prevEl: '.main-wrapper .bt-prev',
+		nextEl: ".main-wrapper .bt-next",
+		prevEl: ".main-wrapper .bt-prev",
 	},
 })
 
