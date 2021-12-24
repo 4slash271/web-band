61d976a018cea767913c45dc414c2838684dadaf
“4slash271” <4slash271@gmail.com>
2021-12-25 03:13:37 +0900

    add kakao map API and js

--- js/index.js
+++ js/index.js
@@ -29,3 +29,18 @@ var swiper = new Swiper(".main-wrapper .swiper-container", {
 	},
 })
 
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var center = new kakao.maps.LatLng(37.555496, 126.936840); //지도의 중심좌표.
+var options = { //지도를 생성할 때 필요한 기본 옵션
+        center :center,
+        level: 2 //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+map.setZoomable(false);
+
+
+window.addEventListener("resize",function(){
+    map.setCenter(center);
+
+})
